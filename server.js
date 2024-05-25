333b60e08e3cc5782c694f964689dfb2825ccc77
aida <aidazimas@hotmail.com>
2024-05-25 16:24:16 +0100

    Update server configuration and CSP directives
    The server.js file has been updated to reflect changes in content security policy directives. Moreover, the 'frameAncestors' directive was replaced with 'frameSrc', and its URL was updated according to the environment.

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import path from 'path';
-import {fileURLToPath} from 'url';
 import helmet from 'helmet';
+import {fileURLToPath} from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 const isDevelopment = process.env.NODE_ENV === 'development';
+
 const cspDirectives = {
     directives: {
         defaultSrc: ["'self'"],
@@ -15,7 +16,7 @@ const cspDirectives = {
         imgSrc: ["'self'", "data:"],
         connectSrc: ["'self'"],
         fontSrc: ["'self'", "https:"],
-        frameAncestors: ["'self'", isDevelopment ? "http://localhost:8090" : "https://your-production-site.com"],
+        frameSrc: ["'self'", isDevelopment ? "http://localhost:8090" : "https://auth.keypractica.com"],
         objectSrc: ["'none'"],
         upgradeInsecureRequests: [],
     },
