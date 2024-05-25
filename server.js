38c24679c6bdcc3a47a8067336395d951beefbd4
aida <aidazimas@hotmail.com>
2024-05-25 19:34:02 +0100

    Implement CSP nonce and simplify middleware in server.js
    The server.js file has been refactored to generate and use a nonce in Content Security Policy. Extra server configuration and a route were also removed to simplify the middleware structure. The .github/workflows/deploying.yml file was updated for clarity, and a new index.ejs file was added.

--- server.js
+++ server.js
@@ -6,21 +6,24 @@ import {fileURLToPath} from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const app = express();
+
 const isDevelopment = process.env.NODE_ENV === 'development';
 
+// Middleware to generate a nonce and add it to res.locals
 app.use((req, res, next) => {
     res.locals.nonce = crypto.randomBytes(16).toString('base64');
     next();
 });
 
+// CSP configuration with nonce
 app.use((req, res, next) => {
     helmet.contentSecurityPolicy({
         directives: {
             defaultSrc: ["'self'"],
-            scriptSrc: ["'self'", `'nonce-${res.locals.nonce}'`, "'strict-dynamic'"],
-            scriptSrcElem: ["'self'", `'nonce-${res.locals.nonce}'`, 'https://www.keypractica.com'],
-            styleSrc: ["'self'", "'unsafe-inline'"],
+            scriptSrc: ["'self'", `'nonce-${res.locals.nonce}'`, 'https://www.keypractica.com'],
+            styleSrc: ["'self'", "'unsafe-inline'", 'https://www.keypractica.com'],
             imgSrc: ["'self'", "data:"],
             connectSrc: ["'self'"],
             fontSrc: ["'self'", "https:"],
@@ -31,22 +34,13 @@ app.use((req, res, next) => {
     })(req, res, next);
 });
 
+// Set EJS as the view engine and set the views directory
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src', 'view'));
-app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/config', (req, res) => {
-    const config = {
-        "realm": process.env.KEYPRACTICA_REALM,
-        "auth-server-url": process.env.KEYPRACTICA_AUTH_SERVER_URL,
-        "resource": process.env.KEYPRACTICA_RESOURCE,
-        "client-realm": "semantyca.com"
-    };
-    res.json(config);
-});
-
+// Serve the HTML file with injected nonce and dynamic title
 app.get('*', (req, res) => {
-    const title = "kneox";
+    const title = "kneox"; // Set your dynamic title here
     res.render('index', { nonce: res.locals.nonce, title });
 });
 
