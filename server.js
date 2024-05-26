1fefd681b46b0dedc69767105dbf0a65a6b4324a
aida <aidazimas@hotmail.com>
2024-05-26 10:39:44 +0100

    Implement Helmet for CSP settings and update favicon
    The commit applies the Helmet library to set up Content Security Policy (CSP) headers, optimizing response security. It also adds a new favicon (kx.svg) and updates index.ejs to use the new favicon.

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ import path from 'path';
 import crypto from 'crypto';
 import {fileURLToPath} from 'url';
 import fs from 'fs';
+import helmet from "helmet";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const app = express();
 
 console.log('Starting server...');
@@ -26,31 +26,38 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    console.log('Setting CSP headers manually');
-    res.setHeader("Content-Security-Policy", `default-src 'self'; script-src 'self' 'nonce-${res.locals.nonce}' https://www.keypractica.com; style-src 'self' 'unsafe-inline' https://www.keypractica.com; img-src 'self' data:; connect-src 'self'; font-src 'self' https:; frame-src 'self' https://auth.keypractica.com; frame-ancestors 'self' https://auth.keypractica.com; object-src 'none'; upgrade-insecure-requests; base-uri 'self'; form-action 'self';`);
-    next();
-});
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: ["'self'"],
+            scriptSrc: ["'self'", 'https://www.keypractica.com'],
+            styleSrc: ["'self'", 'https://www.keypractica.com'],
+            imgSrc: ["'self'", 'data:'],
+            connectSrc: ["'self'"],
+            fontSrc: ["'self'", 'https:'],
+            frameSrc: ["'self'", 'https://auth.keypractica.com'],
+            frameAncestors: ["'self'", 'https://auth.keypractica.com'],
+            objectSrc: ["'none'"],
+            upgradeInsecureRequests: [],
+            baseUri: "'self'",
+            formAction: "'self'"
+        },
+        reportOnly: false,
+        reportUri: ''
+    })
+);
 
-app.use((req, res, next) => {
-    res.setHeader('X-Custom-Header', 'CSP-Updated');
-    next();
-});
-
-// Set EJS as the view engine and set the views directory to the current directory
 app.set('view engine', 'ejs');
-app.set('views', __dirname); // Views directory set to current directory
+app.set('views', __dirname);
 console.log(`Views directory set to: ${__dirname}`);
 
-// Serve static files from the 'dist' directory
 const staticPath = path.join(__dirname, 'dist');
-app.use('/assets', express.static(staticPath)); // Ensure correct path
+app.use('/assets', express.static(staticPath));
 console.log(`Static files served from: ${staticPath}`);
 
-// Serve the HTML file with injected nonce and dynamic title
 app.get('*', (req, res) => {
-    const title = "kneox"; // Set your dynamic title here
-    const mainJs = manifest['src/main.ts']?.file || 'assets/index-qhnKbt7S.js'; // Default fallback
+    const title = "kneox";
+    const mainJs = manifest['src/main.ts']?.file || 'assets/index-xxxxxx.js';
     console.log(`Rendering index.ejs with title: ${title}, nonce: ${res.locals.nonce}, and script: ${mainJs}`);
     res.render('index', {nonce: res.locals.nonce, title, mainJs});
 });
