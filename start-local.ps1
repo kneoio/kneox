4d1781f26abbfc4e3a6d63e06be5419c6e4f1611
kaira <justaidajam@gmail.com>
2024-05-28 09:37:07 +0100

    merge

---
+++ start-local.ps1
@@ -0,0 +1,9 @@
+$env:NODE_ENV = "production"
+$env:PORT = "46220"
+$env:KEYPRACTICA_REALM = "keypractica_auth"
+$env:KEYPRACTICA_AUTH_SERVER_URL = "https://auth.keypractica.com"
+$env:KEYPRACTICA_RESOURCE = "keypractica_projects"
+
+$nodePath = "node.exe"
+
+& $nodePath server.js
