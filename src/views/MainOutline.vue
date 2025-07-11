100c1e1a736b2f0c29c882ecd623d6e12eed91f1
kaira <justaidajam@gmail.com>
2025-07-11 23:09:33 +0100

    #

--- src/views/MainOutline.vue
+++ src/views/MainOutline.vue
@@ -13,7 +13,7 @@
         :class="{ 'drawer-open': isDrawerOpen }"
     >
       <div class="drawer-header">
-        <n-space><n-h2>Mixpla</n-h2><n-h6 style="color:#6c757d; font-size: small">manager v.1.7.4</n-h6></n-space>
+        <n-space><n-h2>Mixpla</n-h2><n-h6 style="color:#6c757d; font-size: small">manager v.1.7.5</n-h6></n-space>
       </div>
       <div class="drawer-content" style="overflow-y: auto; max-height: calc(100vh - 80px);">
         <n-menu
