5a47562068c1dfc4d040d823ebccf65d3fb4c468
kaira <justaidajam@gmail.com>
2025-07-24 12:11:49 +0100

    #

--- src/views/MixplaView.vue
+++ src/views/MixplaView.vue
@@ -50,7 +50,7 @@
             </div>
             <a v-else v-for="station in stationsData" 
                :key="station.name" 
-               :href="`/player/?radio=${encodeURIComponent(station.name.toLowerCase())}`" 
+               :href="`https://mixpla.kneo.io/index.html?radio=${encodeURIComponent(station.name.toLowerCase())}`" 
                target="_blank" 
                rel="noopener noreferrer" 
                class="block pt-2 group relative pl-5">
