4f747e9372cd793a6ca3e6a93d11451ef24dd0ba
kaira <justaidajam@gmail.com>
2026-03-13 22:49:27 +0000

    #

--- src/keycloakFactory.js
+++ src/keycloakFactory.js
@@ -1,12 +1,9 @@
 import Keycloak from 'keycloak-js';
 
 const cfg = {
-    //url: 'https://auth.kneo.io/auth', // Corrected URL
     url: 'https://auth.semantyca.com',
     realm: 'mixpla',
-    clientId: 'mixpla_web',
-    // checkLoginIframe: false, // See note below
-    // onLoad: 'login-required' // This is overridden by main.js anyway
+    clientId: 'mixpla_web' 
 };
 
 const keycloakInst = new Keycloak(cfg);
